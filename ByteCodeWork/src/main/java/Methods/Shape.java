85f81e89ed0eaf1f75db4314025e1f033dbb0be3
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-09 23:28:31 +0530

    Shape

---
+++ ByteCodeWork/src/main/java/Methods/Shape.java
@@ -0,0 +1,18 @@
+package Methods;
+class shapes{
+	void draw() {
+		System.out.println("Draw a shape");
+	}
+}
+class circle extends shapes{
+	void draws() {
+		System.out.println("Draw a circle");
+	}
+}
+public class Shape{
+	public  static void main(String[] args) {
+		circle c = new circle();
+		c.draws();
+		
+	}
+}
