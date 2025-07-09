e23360c779973e13da1e830fde3edee94f6b9424
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-09 21:27:31 +0530

    rect

---
+++ ByteCodeWork/src/main/java/Methods/Rectangle.java
@@ -0,0 +1,20 @@
+package Methods;
+class Shape{
+    double length;
+    double breadth;
+    void setdim(double l,double b){
+        length=l;
+        breadth=b;
+    }
+}
+class Rectangle extends Shape{
+    void area(){
+            double a=length*breadth;
+            System.out.print("Area = "+a);
+        }
+    public static void main(String[]args){
+        Rectangle r = new Rectangle();
+        r.setdim(5,30);
+        r.area();
+    }
+}
