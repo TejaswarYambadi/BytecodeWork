e36b03a45e3cdc3d59cb2bc47bfe25611eae18c6
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-09 18:19:20 +0530

    First commit in method

--- ByteCodeWork/src/main/java/project/DepartmentalPerformanceAnalyzer.java
+++ ByteCodeWork/src/main/java/project/DepartmentalPerformanceAnalyzer.java
@@ -1,8 +1,33 @@
 package project;
+import java.util.*;
 
 public class DepartmentalPerformanceAnalyzer {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no of Departments = ");
+        int d = sc.nextInt();
 
+        System.out.print("Enter no of students per each department = ");
+        int sped = sc.nextInt();
+
+        System.out.print("Enter number of subjects = ");
+        int nos = sc.nextInt();
+        sc.nextLine();
+
+        for (int de = 0; de < d; de++) {
+            System.out.println("Department " + (de + 1) + " ");
+            Object[][] dm = new Object[sped][nos + 1];
+            for (int i = 0; i < sped; i++) {
+                System.out.print("Enter student " + (i + 1) + " name = ");
+                dm[i][0] = sc.nextLine();
+                System.out.println("Enter marks: ");
+                for (int j = 0; j < nos; j++) {
+                    dm[i][j + 1] = sc.nextFloat();
+                }
+                sc.nextLine();
+            }
+        }
+        sc.close();
+    }
 }
+
