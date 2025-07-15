39d2c5ac332e3b6fa2ab337895edbb07340bf0b8
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-15 09:45:58 +0530

    PaymentDone

---
+++ ByteCodeWork/src/main/java/interfaces/PaymentDemo.java
@@ -0,0 +1,24 @@
+package interfaces;
+
+interface Payment{
+	void processPayment(double amount);
+}
+class PayPal implements Payment{
+	public void processPayment(double amount) {
+		System.out.println("Processing of amount "+ amount+" through Paypal");
+	};
+}
+class Stripe implements Payment{
+	public void processPayment(double amount) {
+		System.out.println("Processing of amount "+ amount+" through Stripe");
+	};
+}
+
+public class PaymentDemo {
+	public static void main(String[] args){
+		PayPal p1 = new PayPal();
+		Stripe s1 = new Stripe();
+		p1.processPayment(20);
+		s1.processPayment(50);
+	}
+}
