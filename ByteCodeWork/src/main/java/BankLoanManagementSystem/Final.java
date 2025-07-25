5703ddddbd319db12d15b85bf930dbe342634471
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-25 21:47:22 +0530

    Okay

---
+++ ByteCodeWork/src/main/java/BankLoanManagementSystem/Final.java
@@ -0,0 +1,11 @@
+package BankLoanManagementSystem;
+
+public class Final {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserManagementSystem umc= new UserManagementSystem();
+		umc.display();
+	}
+
+}
---
+++ ByteCodeWork/src/main/java/BankLoanManagementSystem/UserManagementSystem.java
@@ -0,0 +1,10 @@
+package BankLoanManagementSystem;
+import java.util.Scanner;
+public class UserManagementSystem {
+	Scanner sc = new Scanner(System.in);
+	public void display() {
+		System.out.print("Choose your Option either ");
+		System.out.print("1 for Registration and 2 for Login");
+		int choice ;
+	}
+}
