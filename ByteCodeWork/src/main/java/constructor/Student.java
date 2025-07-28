7b27708cae22a6af297150bd1bc5bf244cb090f5
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-28 12:04:49 +0530

    Student done

--- ByteCodeWork/src/main/java/constructor/Student.java
+++ ByteCodeWork/src/main/java/constructor/Student.java
@@ -1,16 +1,19 @@
 package constructor;
 
 public class Student {
-	String name="unknown";
+	String name;
+	Student(){
+		name="unknown";
+	}
 	Student(String name){
-		name =this.name;
+	this.name=name;  
 	}
 	public String names() {
 		return name;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student s1 = new Student("Hiii");
+		Student s1 = new Student("hii");
 		System.out.println(s1.names());
 	}
 
