fc5456a344d8a7a2d4f77425c01640d7887017e7
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-28 12:31:40 +0530

    Multiple Done

--- ByteCodeWork/src/main/java/constructor/Multiple.java
+++ ByteCodeWork/src/main/java/constructor/Multiple.java
@@ -21,9 +21,14 @@ public class Multiple {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Multiple m = new Multiple();
-		m.display1();
-		m.display2();
-
+		System.out.println(m.display1());
+		System.out.println(m.display2());
+		Multiple m1 = new Multiple(10);
+		System.out.println(m1.display1());
+		System.out.println(m1.display2());
+		Multiple m2 = new Multiple(10,20);
+		System.out.println(m2.display1());
+		System.out.println(m2.display2());
 	}
 
 }
