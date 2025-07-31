d777f2dc7f7d759f4129b6ab77a6b04ce1fb2113
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-31 21:44:21 +0530

    Task4 Commited

--- ByteCodeWork/src/main/java/encapsulation/Task4.java
+++ ByteCodeWork/src/main/java/encapsulation/Task4.java
@@ -1,10 +1,29 @@
 package encapsulation;
-
+class LightBulb {
+	private boolean isOn;
+	
+	public LightBulb() {
+        isOn = false;
+    }
+	public void setTurnOn() {
+		isOn=true;
+	}
+	public void setTurnOff() {
+		isOn=false;
+	}
+	public boolean isOn() {
+		return isOn;
+	}
+}
 public class Task4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LightBulb b = new LightBulb();
+		System.out.println(b.isOn());
+		b.setTurnOn();
+		System.out.println(b.isOn());
+		b.setTurnOff();
+		System.out.println(b.isOn());
 	}
 
 }
