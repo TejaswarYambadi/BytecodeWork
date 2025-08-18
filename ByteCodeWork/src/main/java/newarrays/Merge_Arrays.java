84f118723bb3af98f3510d5dc71b987a7a240ed1
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-08-18 12:38:31 +0530

    done

---
+++ ByteCodeWork/src/main/java/newarrays/Merge_Arrays.java
@@ -0,0 +1,19 @@
+//6.Write a program to merge two arrays.
+package newarrays;
+public class Merge_Arrays {
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5};
+		int b[]= {6,7,8,9};
+		int c[] = new int[a.length+b.length];
+		for(int j=0;j<a.length;j++) {
+			c[j]=a[j];
+		}
+		for(int i =0;i<b.length;i++) {
+			c[a.length+i]=b[i];
+		}
+		for(int i=0;i<c.length;i++) {
+			System.out.print(c[i]+" ");
+		}
+	}
+
+}
