44a29907053410ed162f6e57cca848afe7148796
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-08-22 12:32:13 +0530

    done

---
+++ ByteCodeWork/src/main/java/exam/Task.java
@@ -0,0 +1,43 @@
+package exam;
+public class Task {
+	public static void main(String[] args) {
+		int[][] matrix = {
+			    { 1,  2,  3,  4},
+			    { 5,  6,  7,  8},
+			    { 9, 10, 11, 12},
+			    {13, 14, 15, 16}
+			};
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+		System.out.println(rows);
+		System.out.println(cols);
+		int top=0;
+		int bottom= rows-1;
+		int left =0;
+		int right = cols-1;
+		
+		while(top<=bottom && left <=right) {
+			for(int i=left;i<=right;i++) {
+				System.out.print(matrix[top][i]+" ");
+			}
+			top++;
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+		}
+		
+	}
+
+}
--- ByteCodeWork/src/main/java/exam/Task4.java
+++ ByteCodeWork/src/main/java/exam/Task4.java
@@ -2,40 +2,25 @@ package exam;
 public class Task4 {
 	public static void main(String[] args) {
 		int[][] matrix = {
-			    { 1,  2,  3,  4},
-			    { 5,  6,  7,  8},
-			    { 9, 10, 11, 12},
-			    {13, 14, 15, 16}
+			    { 1,  2,  3},
+			    { 4,  5,  6},
+			    { 7,  8,  9}
 			};
-		int rows = matrix.length;
-		int cols = matrix[0].length;
-		System.out.println(rows);
-		System.out.println(cols);
 		int top=0;
-		int bottom= rows-1;
-		int left =0;
-		int right = cols-1;
-		
-		while(top<=bottom && left <=right) {
-			for(int i=left;i<=right;i++) {
-				System.out.print(matrix[top][i]+" ");
-			}
-			top++;
-            for (int i = top; i <= bottom; i++) {
-                System.out.print(matrix[i][right] + " ");
-            }
-            right--;
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    System.out.print(matrix[bottom][i] + " ");
-                }
-                bottom--;
-            }            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    System.out.print(matrix[i][left] + " ");
-                }
-                left++;
-            }
+		int right=3;
+		int bottom=3;
+		int left=0;
+		for(int i=top;i<right;i++) {
+			System.out.print(matrix[0][i]+" ");
+		}
+		for(int i=1;i<bottom;i++) {
+			System.out.print(matrix[i][2]+" " );
+		}
+		for(int i=1;i>=left;i--) {
+			System.out.print(matrix[2][i]+" ");
+		}
+		for(int i=0;i<2;i++) {
+			System.out.print(matrix[1][i]+" ");
 		}
 		
 	}
