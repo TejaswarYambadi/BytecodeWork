929db8e85c54c5e724f03729a478d9806c8a1209
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-09-12 10:14:25 +0530

    Processing first web project

---
+++ FirstWebProject/src/main/java/BasicServlet.java
@@ -0,0 +1,33 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.*;
+/**
+ * Servlet implementation class BasicServlet
+ */
+@WebServlet("/BasicServlet")
+public class BasicServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public BasicServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("Hello, this is a basic servlet response!");
+	}
+
+}
